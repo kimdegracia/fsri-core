0e2cfa4cba393aa6fc1d77cd0864148e4c2b13ae
Joe Willi <willijm92@gmail.com>
2019-01-29 18:26:31 -0500

    Completed example script for part 2 of wiki page collection

--- Project_Repo_Example/3_Scripts/example_scripts/pt2_csv_to_df.py
+++ Project_Repo_Example/3_Scripts/example_scripts/pt2_csv_to_df.py
@@ -3,7 +3,7 @@
 # ***************************** Run Notes ***************************** #
 # - This file should be used in conjunction with the fsri-core wiki     #
 #       page titled "Python Examples: pt2_csv_to_df.py", which          #
-#       describes _________________                                     #
+#       introduces the use of the Pandas library                        #
 #                                                                       #
 # - It provides the reader of the wiki with a concrete examples of      #
 #       various topics that are discussed & should be used to follow    #
@@ -11,11 +11,13 @@
 #                                                                       #
 # - After properly editing this script based on instructions outlined   #
 #       on the wiki page, execution of this script will:                #
-#                                           
+#           + Load .csv files to be used in the code                    #
+#           + Set the index of a DataFrame                              #
+#           + Load files from the data directory as DataFrames          #
 # ********************************************************************* #
 
 # --------------- #
-# Import packages #
+# Import Packages #
 # --------------- #
 import os
 import pandas as pd
@@ -44,25 +46,29 @@ df_no_spec_idx = pd.read_csv(____+'________________')
 df_idx_spec = pd.read_csv(____+'________________', index_col='_______')
 
 # print both dataframes to compare differences
-print(df_no_spec_idx)
-print(df_idx_spec)
-exit()
+print(___________)
+print(___________)
 
-# Assign the channel column as index in df_no_spec_idx
+# ------------------------------- #
+# Set Index in Existing DataFrame #
+# ------------------------------- #
+# Define channel_list as df_no_spec_idx with 'Channel' column as index
+__________ = ___________.set_index(_______)
+print(___________)
+print(___________)
+
+# ------------------ #
+# Using .iloc & .loc #
+# ------------------ #
+# Print only the rows in channel_list that are of type temperature
+print(_________.loc[_____________, :])
+
+# ----------------------- #
+# Loop Through Data Files #
+# ----------------------- #
+for f in __.______(________):
+    # Print the file name
 
 
-# Compare dfs to show assignment was effective, use df.head()
+    # Read in file & print first 4 rows of DataFrame
 
-# Print df.head(), df.tail()
-
-# Show how index can be changed
-
-# Show how index can be specified when reading in file
-
-# Show how columns can be renamed
-
-# Use of df.describe()
-
-# Show how to print column labels, index values, etc.
-
-# Use for loop to loop through data files, will analyze data in Part 3
